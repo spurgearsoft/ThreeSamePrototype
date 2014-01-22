fdf30908a1101fae18b4c447e1e33f6322a41310
흰둥이 <side4u@side-macbook.local>
2014-01-22 23:49:46 +0900

    new

--- ThreeSamePrototype/ViewController.m
+++ ThreeSamePrototype/ViewController.m
@@ -14,11 +14,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    
     // Configure the view.
     SKView * skView = (SKView *)self.view;
     skView.showsFPS = YES;
     skView.showsNodeCount = YES;
+    
     
     // Create and configure the scene.
     SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
